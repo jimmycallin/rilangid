9f6714aac4d77d902a1aef2eea2c57bf3b68847e
Jimmy Callin <jimmy.callin@gmail.com>
2015-02-05 17:20:25 +0100

    made changes to project

--- rilangid/evaluate.py
+++ rilangid/evaluate.py
@@ -1,7 +1,7 @@
 from models import *
 import os
 import re
-import pyresult
+import expy
 import pydsm
 import sys
 from importlib import import_module
@@ -9,14 +9,15 @@ from pprint import pprint
 
 languages = ['eng', 'bul', 'nld', 'fin', 'lav', 'lit', 'ron', 'deu', 'por', 'slk', 'slv', 'ita', 'spa', 'pol', 'hun', 'ell', 'dan', 'fra', 'est', 'ces', 'afr', 'swe']
 
-def load_project(name, test_sentences, assert_clean_repo=True):
-    try:
-        proj = pyresult.Project.get_project(name=name, assert_clean_repo=assert_clean_repo)
-        print("Loaded existing project {}.".format(name))
-    except KeyError:
-        proj = pyresult.Project(name=name,
-                                test_data=test_sentences, assert_clean_repo=assert_clean_repo)
-        print("Created new project {}.".format(name))
+def load_project(configuration, assert_clean_repo=False):
+    project_name = configuration.pop('project_name')
+    test_sentences = load_test_sentences(configuration)
+    proj = expy.Project.search_project(project_name=project_name, assert_clean_repo=assert_clean_repo)
+    if proj:
+        print("Loaded existing project {}.".format(project_name))
+    else:
+        proj = expy.Project(project_name=project_name, test_data=test_sentences, assert_clean_repo=assert_clean_repo)
+        print("Created new project {}.".format(project_name))
     return proj
 
 def get_corpora(languages):
@@ -25,14 +26,14 @@ def get_corpora(languages):
         corpora[language] = open('resources/train/reproduce/{}.txt'.format(language))
     return corpora
 
-def load_test_sentences(path, languages):
+def load_test_sentences(configuration):
     sentences = {}
-    path_content = os.listdir(path)
+    path_content = os.listdir(configuration['test_path'])
     for f in path_content:
         if re.match('(\w+)_\d+_p.txt', f):
             lang = re.findall('(\w+)_\d+_p.txt$', f)[0]
-            if lang in languages:
-                with open(path + f) as content:
+            if lang in configuration['languages']:
+                with open(configuration['test_path'] + f) as content:
                     sentences[content.readline().strip()] = lang
 
     return sentences
@@ -50,19 +51,21 @@ def evaluate(model_path, test_sentences):
     return pred_results
 
 def run_experiment(configuration):
-    test_sentences = load_test_sentences(configuration['test_path'], configuration['languages'])
-    proj = load_project(configuration['project_name'], test_sentences, assert_clean_repo=configuration.get('assert_clean_repo', True))
+    proj = load_project(configuration, assert_clean_repo=configuration.pop('assert_clean_repo', True))
+    test_sentences = proj.test_data
     corpora = get_corpora(configuration['languages'])
 
-    print("Starting training with configuration:")
+    print("Configuration:")
     pprint(configuration, width=80)
     if configuration.get('train', True):
+        print("Starting training...")
         model = configuration['rimodel'](config=configuration)
         model.train(corpora)
     print("Evaluating model...")
     
     sentence_results = evaluate(configuration['store_path'], test_sentences)
     experiment = proj.new_experiment(predicted=sentence_results,
+                                     tags=configuration.pop('tags', None),
                                      configuration=configuration,
                                      description=configuration['description'])
 
